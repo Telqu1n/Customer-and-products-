438aa690112b6a8e569b0c24a39db452862a337a
Jack Carney <jackcarney09@outlook.com>
2024-12-05 14:02:32 +0000

    creating a sup class for meta

--- customer_app/models.py
+++ customer_app/models.py
@@ -18,8 +18,13 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.FloatField(max_length=10)
     
+    
+    class Meta:
+        verbose_name_plural = 'Products
     
     def __str__(self):
         return f'{self.name}, {self.price}'
+    
+
