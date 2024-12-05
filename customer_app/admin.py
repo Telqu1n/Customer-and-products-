b62c0e27fb3a92ead3e053c0d5f7697760cb265e
Jack Carney <jackcarney09@outlook.com>
2024-12-05 13:04:20 +0000

    changing the look of the admin panel

--- customer_app/admin.py
+++ customer_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Customer, Product
 
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display =('full_name', 'email', 'phone')
+    list_filter = ('last_name', 'email', 'phone')
+    
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    list_filter = ('name', 'price')    
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product, ProductAdmin)
