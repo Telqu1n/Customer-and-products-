ae38d2b40ee0bcc7feb1c616a654f2412c509ba0
Jack Carney <jackcarney09@outlook.com>
2024-12-12 10:45:55 +0000

    setting up manyToMany database between products and customer

--- customer_app/models.py
+++ customer_app/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Customer(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
-
-    def full_name(self):
-        return f'{self.first_name} {self.last_name}'
-    
-    
-    def __str__(self):
-        return self.full_name()
-
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -22,9 +9,23 @@ class Product(models.Model):
     
     
     class Meta:
-        verbose_name_plural = 'Products
+        verbose_name_plural = 'Products'
     
     def __str__(self):
         return f'{self.name}, {self.price}'
     
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    Product = models.ManyToManyField(Product)
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    
+    def __str__(self):
+        return self.full_name()
+
 
